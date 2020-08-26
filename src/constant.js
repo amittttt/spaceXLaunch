3b83e1902083d49a06788a87fd1b12699999375a
Amit <amittttmishra@gmail.com>
2020-08-26 13:20:58 +0530

    Update constant.js

--- src/constant.js
+++ src/constant.js
@@ -1,2 +1,2 @@
-export const BASE_API = "https://api.spacexdata.com/v3/launches?limit=100";
+export const BASE_API = "https://api.spacexdata.com/v3/launches?limit=50";
 export const FallBack_IMAGE_URL = "https://images2.imgbox.com/40/e3/GypSkayF_o.png";
